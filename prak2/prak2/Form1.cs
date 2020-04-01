ee87279a76b83c0da0dc6785ee5daad8633589dc
paintInSour <55554844+paintInSour@users.noreply.github.com>
2020-04-01 09:48:34 +0300

    Add files via upload

---
+++ prak2/prak2/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace prak2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void OnFormLoad(object obj,EventArgs e)
+        {
+            List<Operation> operations = new List<Operation> { 
+                new Add(),
+                new Subtract(),
+                new Multiply(),
+                new Divide() };
+
+            foreach(Operation operation in operations)
+            {
+                richTextBox1.Text += operation.GetInfo() + "\n";
+            }
+        }
+        public void OnButtonClick(object obj, EventArgs e)
+        {
+            using (StreamReader streamReader = new StreamReader("myFile.txt"))
+            {
+                List<string> elements = streamReader.ReadLine().Split(' ').ToList();
+                Operation operation ;
+                switch (elements[1])
+                {
+                    case "+":
+                        operation = new Add();
+                        break;
+                    case "-":
+                        operation = new Subtract();
+                        break;
+                    case "*":
+                        operation = new Multiply();
+                        break;
+                    case "/":
+                        operation = new Divide();
+                        break;
+                    default:
+                        operation = new Divide();
+                        break;
+
+                }
+                operation.operator_1 = Convert.ToDouble(elements[0]);
+                operation.operator_2 = Convert.ToDouble(elements[2]);
+                richTextBox1.Clear();
+                richTextBox1.Text = operation.Execute().ToString();
+            } 
+
+        }
+    }
+}
---
+++ prak2/prak2/Operation.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prak2
+{
+    abstract class Operation
+    {
+        public double operator_1;
+        public double operator_2;
+        public abstract double Execute();
+        public string GetInfo()
+        {
+            return nameof(Operation);
+        }
+    }
+
+     class Add : Operation
+    {
+        public sealed override double Execute()
+        {
+            return operator_1 + operator_2;
+        }
+        public new string GetInfo()
+        {
+            return base.GetInfo();
+        }
+
+
+    }
+
+    class Subtract:Operation
+    {
+        public override double Execute()
+        {
+            return operator_1 - operator_2;
+        }
+        public new string GetInfo()
+        {
+            return nameof(Subtract);
+        }
+    }
+     class Multiply:Operation
+    {
+        public override double Execute()
+        {
+            return operator_1 * operator_2;
+        }
+        public new string GetInfo()
+        {
+            return nameof(Multiply);
+        }
+    }
+    class Divide:Operation
+    {
+        public override double Execute()
+        {
+            return operator_1 / operator_2;
+        }
+        public new string GetInfo()
+        {
+            return nameof(Divide);
+        }
+    }
+}
